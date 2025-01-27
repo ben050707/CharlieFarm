bed9d971dec46671b2ba69d248fa1091b1b58db7
ben050707 <ben050707@hotmail.com>
2025-01-27 08:52:10 +0000

    movement for character complete

--- Charlie.py
+++ Charlie.py
@@ -275,7 +275,8 @@ class Coby(Enemy):
             if dlclosed:
                 self.pos = 3
                 self.image = self.rest
-        super().move()
+        else:
+            super().move()
 
         
 
